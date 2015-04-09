b5e70884d8e160cdc88b285e864de993c5c3a017
Anisha Etienne <anisha.etienne@icloud.com>
2015-04-08 23:29:16 -0600

    production changes for devise email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.force_ssl = true
+
+  #heroku
+  config.action_mailer.default_url_options = { host: 'hidden-savannah-8865.herokuapp.com' }
 end
